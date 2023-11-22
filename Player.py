eed7fa85df54c2331e03a1daa19a0c9545dabf08
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-22 17:56:38 -0500

    Player class moved to separate file

---
+++ Player.py
@@ -0,0 +1,62 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("images/stormtrooper.png")
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.change_x = 0
+        self.change_y = 0
+        self.level = None
+
+    def update(self):
+        self.calc_grav()
+        self.rect.x += self.change_x
+        block_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
+        for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                self.rect.left = block.rect.right
+
+        self.rect.y += self.change_y
+
+        block_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
+
+            self.change_y = 0
+
+    def calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += 0.35
+
+    def jump(self):
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(
+            self, self.level.platform_list, False
+        )
+        self.rect.y -= 2
+
+        if len(platform_hit_list) > 0:
+            self.change_y = -10
+
+    def go_left(self):
+        self.change_x = -5
+
+    def go_right(self):
+        self.change_x = 5
+
+    def stop(self):
+        self.change_x = 0
--- game.py
+++ game.py
@@ -1,11 +1,7 @@
 import pygame
 from random import randint
+from Player import Player
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
@@ -18,74 +14,8 @@ class Ewok(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
     def placement(self):
-        self.rect.x = randint(50, SCREEN_WIDTH - 50)
-        self.rect.y = randint(50, SCREEN_HEIGHT - 200)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("images/stormtrooper.png")
-        self.image.set_colorkey((255, 255, 255))
-        self.rect = self.image.get_rect()
-        self.change_x = 0
-        self.change_y = 0
-        self.level = None
-
-    def update(self):
-        self.calc_grav()
-        self.rect.x += self.change_x
-        block_hit_list = pygame.sprite.spritecollide(
-            self, self.level.platform_list, False
-        )
-        for block in block_hit_list:
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            elif self.change_x < 0:
-                self.rect.left = block.rect.right
-
-        self.rect.y += self.change_y
-
-        block_hit_list = pygame.sprite.spritecollide(
-            self, self.level.platform_list, False
-        )
-        for block in block_hit_list:
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            elif self.change_y < 0:
-                self.rect.top = block.rect.bottom
-
-            self.change_y = 0
-
-    def calc_grav(self):
-        """Calculate effect of gravity."""
-        if self.change_y == 0:
-            self.change_y = 1
-        else:
-            self.change_y += 0.35
-
-        if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
-            self.change_y = 0
-            self.rect.y = SCREEN_HEIGHT - self.rect.height
-
-    def jump(self):
-        self.rect.y += 2
-        platform_hit_list = pygame.sprite.spritecollide(
-            self, self.level.platform_list, False
-        )
-        self.rect.y -= 2
-
-        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
-            self.change_y = -10
-
-    def go_left(self):
-        self.change_x = -5
-
-    def go_right(self):
-        self.change_x = 5
-
-    def stop(self):
-        self.change_x = 0
+        self.rect.x = randint(50, 750)
+        self.rect.y = randint(50, 400)
 
 
 class Platform(pygame.sprite.Sprite):
@@ -192,7 +122,7 @@ def main():
 
         current_level.draw(screen)
         active_sprite_list.draw(screen)
-        text = font.render("Score: " + str(score), True, WHITE)
+        text = font.render("Score: " + str(score), True, (255, 255, 255))
         screen.blit(text, [10, 10])
 
         clock.tick(60)
