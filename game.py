a23ce54d07a25f1ecfce1eadc56bcdf2ef90d4e7
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-22 19:12:08 -0500

    major refactor of main() function

--- game.py
+++ game.py
@@ -40,11 +40,13 @@ class Level(object):
         self.platform_list.draw(screen)
 
 
-class Level_01(Level):
+class GameLevel(Level):
     def __init__(self, player):
-        Level.__init__(self, player)
+        self.platform_list = pygame.sprite.Group()
+        self.player = player
+        self.background = pygame.image.load("images/background.jpg")
 
-        level = [
+        level_platforms = [
             [100, 200],
             [100, 450],
             [350, 570],
@@ -53,12 +55,18 @@ class Level_01(Level):
             [600, 200],
         ]
 
-        for platform in level:
+        for platform in level_platforms:
             block = Platform()
             block.rect.x = platform[0]
             block.rect.y = platform[1]
-            block.player = self.player
             self.platform_list.add(block)
+
+    def update(self):
+        self.platform_list.update()
+
+    def draw(self, screen):
+        screen.blit(self.background, (0, 0))
+        self.platform_list.draw(screen)
 
 
 def reset(player, screen, current_level, active_sprite_list):
@@ -75,7 +83,6 @@ def reset(player, screen, current_level, active_sprite_list):
     )
     pygame.display.flip()
 
-    # Wait for 5 seconds
     start_ticks = pygame.time.get_ticks()
     while pygame.time.get_ticks() - start_ticks < 5000:
         for event in pygame.event.get():
@@ -83,59 +90,41 @@ def reset(player, screen, current_level, active_sprite_list):
                 pygame.quit()
                 return
 
-    # Reset the player position and clear the screen
     player.rect.x = 400
     player.rect.y = SCREEN_HEIGHT - player.rect.height - 100
     screen.fill((0, 0, 0))  # Clear the screen
-    current_level.draw(screen)  # Redraw the level
+    current_level.draw(screen)
     active_sprite_list.draw(screen)  # Redraw the sprites
     pygame.display.flip()
-
     SCORE = 0
 
 
-def main():
+def handle_events(player):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.go_left()
+            if event.key == pygame.K_RIGHT:
+                player.go_right()
+            if event.key == pygame.K_UP:
+                player.jump()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT and player.change_x < 0:
+                player.stop()
+            if event.key == pygame.K_RIGHT and player.change_x > 0:
+                player.stop()
+    return False
+
+
+def game_loop(screen, clock, font, player, ewok, game_level, active_sprite_list):
     global SCORE
-    pygame.init()
-    font = pygame.font.Font(None, 36)
-    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Platformer Jumper")
-    ewok = Ewok()
-    player = Player()
-    level_list = []
-    level_list.append(Level_01(player))
-    current_level_no = 0
-    current_level = level_list[current_level_no]
-    active_sprite_list = pygame.sprite.Group()
-    player.level = current_level
-    ewok.placement()
-    active_sprite_list.add(ewok)
-    player.rect.x = 400
-    player.rect.y = SCREEN_HEIGHT - player.rect.height - 100
-    active_sprite_list.add(player)
     done = False
-    clock = pygame.time.Clock()
-
-    # -------- Main Program Loop -----------
     while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.go_left()
-                if event.key == pygame.K_RIGHT:
-                    player.go_right()
-                if event.key == pygame.K_UP:
-                    player.jump()
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT and player.change_x < 0:
-                    player.stop()
-                if event.key == pygame.K_RIGHT and player.change_x > 0:
-                    player.stop()
+        done = handle_events(player)
 
         if pygame.sprite.collide_rect(player, ewok):
             SCORE += 1
@@ -143,24 +132,45 @@ def main():
             ewok.placement()
 
         active_sprite_list.update()
-        current_level.update()
+        game_level.update()
 
         if player.rect.right > SCREEN_WIDTH:
             player.rect.right = SCREEN_WIDTH
-
         if player.rect.left < 0:
             player.rect.left = 0
-
         if player.rect.top > SCREEN_HEIGHT + 50:
-            reset(player, screen, current_level, active_sprite_list)
+            reset(player, screen, game_level, active_sprite_list)
 
-        current_level.draw(screen)
+        game_level.draw(screen)
         active_sprite_list.draw(screen)
-        text = font.render("Ewoks caught: " + str(SCORE), True, (255, 255, 255))
-        screen.blit(text, [10, 10])
+        score_text = font.render("Ewoks caught: " + str(SCORE), True, (255, 255, 255))
+        screen.blit(score_text, [10, 10])
 
         clock.tick(60)
         pygame.display.flip()
+
+
+def main():
+    pygame.init()
+    font = pygame.font.Font(None, 36)
+    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Platformer Jumper")
+
+    player = Player()
+    game_level = GameLevel(player)
+    player.level = game_level
+
+    active_sprite_list = pygame.sprite.Group()
+    ewok = Ewok()
+    ewok.placement()
+    active_sprite_list.add(ewok)
+    player.rect.x = 400
+    player.rect.y = SCREEN_HEIGHT - player.rect.height - 100
+    active_sprite_list.add(player)
+
+    clock = pygame.time.Clock()
+    game_loop(screen, clock, font, player, ewok, game_level, active_sprite_list)
 
     pygame.quit()
 
