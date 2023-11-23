cb57a3174017e2e8175f0ea55f067e67b4b0e4c9
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-22 19:05:39 -0500

    some refactoring

--- Player.py
+++ Player.py
@@ -14,10 +14,11 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.calc_grav()
         self.rect.x += self.change_x
-        block_hit_list = pygame.sprite.spritecollide(
+
+        block_hit_list_x = pygame.sprite.spritecollide(
             self, self.level.platform_list, False
         )
-        for block in block_hit_list:
+        for block in block_hit_list_x:
             if self.change_x > 0:
                 self.rect.right = block.rect.left
             elif self.change_x < 0:
@@ -25,15 +26,14 @@ class Player(pygame.sprite.Sprite):
 
         self.rect.y += self.change_y
 
-        block_hit_list = pygame.sprite.spritecollide(
+        block_hit_list_y = pygame.sprite.spritecollide(
             self, self.level.platform_list, False
         )
-        for block in block_hit_list:
+        for block in block_hit_list_y:
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
             elif self.change_y < 0:
                 self.rect.top = block.rect.bottom
-
             self.change_y = 0
 
     def calc_grav(self):
--- game.py
+++ game.py
@@ -29,18 +29,15 @@ class Platform(pygame.sprite.Sprite):
 class Level(object):
     def __init__(self, player):
         self.platform_list = pygame.sprite.Group()
-        self.enemy_list = pygame.sprite.Group()
         self.player = player
         self.background = pygame.image.load("images/background.jpg")
 
     def update(self):
         self.platform_list.update()
-        self.enemy_list.update()
 
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
         self.platform_list.draw(screen)
-        self.enemy_list.draw(screen)
 
 
 class Level_01(Level):
